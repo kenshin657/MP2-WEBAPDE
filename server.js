8574e8da9be272375439fa074bf61c53254d5a02
Kyle T <kyle_tagle@dlsu.edu.ph>
2019-07-31 18:48:01 +0800

    register now working w/ database

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require("express")
 const app = express()
+const session = require("express-session")
+
+const hbs = require("hbs")
+const cookieparser = require("cookie-parser")
+const mongoose = require("mongoose")
+const url = require("url")
+
 
 const bodyparser = require("body-parser")
 const urlencoder = bodyparser.urlencoded({
     extended:false
 })
 
-app.use(express.static(__dirname+ "/public"))
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost:27017/sample_Db", {
+    useNewUrlParser:true
+})
 
+app.use(cookieparser())
+
+app.use(express.static(__dirname+ "/public"))
+const User = require("./user.js").User
+
+app.use(session({
+    resave: true,
+    name:"cookieMonster",
+    saveUninitialized: true,
+    secret : "secretpass",
+    cookie:{
+        maxAge: 5*60*1000
+    }
+}))
 
 app.get("/", (req, res)=> {
     //req = what the user sent us
@@ -44,6 +68,13 @@ app.post("/register", urlencoder, (req, res)=>{
     })
 })
 
+app.get("/logout", (req,res)=>{
+    req.session.destroy((err)=>{
+        console.log("Error in Logging Out")
+    })
+    res.redirect("/")
+})
+
 app.listen(process.env.PORT || 5000, ()=> {
-    console.log("Webpage is Up at port 3500!")
+    console.log("Webpage is Up at port 5000!")
 })
